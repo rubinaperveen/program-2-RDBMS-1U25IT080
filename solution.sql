c576a6fbbf5819d47b31c0d6afe23fc7f9a85854
RUBINA PARVEEN S <rubinaperveen2022@gmail.com>
2026-08-21 09:51:27 +0530

    program 2 RDBMS 1U25IT080

--- solution.sql
+++ solution.sql
@@ -1,1 +1,10 @@
-
+CREATE DATABASE CLASS;
+USE CLASS;
+CREATE TABLE STtudent (
+  StudentID INT PRIMARY KEY,
+  StudentName VARCHAR (20),
+  DOB INT (20),
+  Gender VARCHAR (10),
+  Department INT(10)
+  );
+desc Student;
